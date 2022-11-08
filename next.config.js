9026f33bf48214333ed567adc443769c6f877e09
abhishekkbxr <abhishekbxr203@gmail.com>
2022-11-08 21:14:40 +0530

    assingment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+module.exports = {
+  images: {
+    domains: ["source.unsplash.com" ,"images.unsplash.com" , "3dmodels.arvillage.com"],
+  },
 
-module.exports = nextConfig
+  env:{
+    "MONGO_URI":"mongodb+srv://abhishek:abhishek@cluster0.pno8r.mongodb.net/assingment?retryWrites=true&w=majority&ssl=true",
+  
+    "NEXT_PUBLIC_HOST":"http://localhost:3000/"
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
